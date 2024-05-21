dbe8acd918163cf8c87968b991d66d9577075396
cvjeff <cvjeffry@gmail.com>
2024-05-20 22:54:59 -0600

    TEMA 1 INTRODUCCIÓN A LOS SISTEMAS OPERATIVOS

---
+++ js/script.js
@@ -0,0 +1,19 @@
+let currentSlide = 0;
+const slides = document.querySelectorAll('.slide');
+
+function showSlide(index) {
+    slides.forEach(slide => slide.classList.remove('active'));
+    slides[index].classList.add('active');
+}
+
+function nextSlide() {
+    currentSlide = (currentSlide + 1) % slides.length;
+    showSlide(currentSlide);
+}
+
+function previousSlide() {
+    currentSlide = (currentSlide - 1 + slides.length) % slides.length;
+    showSlide(currentSlide);
+}
+
+showSlide(currentSlide); // Initialize the first slide
